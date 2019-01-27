6b61447104522f7d5e92c21fb97f6589de547052
Unknown <likdudi@gmail.com>
2019-01-27 09:04:29 -0800

    Condition variable support.

--- src/Condition.h
+++ src/Condition.h
@@ -18,10 +18,33 @@ namespace core{
         Condition();
         void Signal(Command command);
         void Wait(Mutex& mutex);
-        
+    
     private:
         volatile std::atomic<int> m_command;
         std::atomic<int> m_waitersCount;
         Mutex m_wakeLock;
     };
+    
+    class ConditionVariable
+    {
+    public:
+        ConditionVariable();
+        ConditionVariable(const ConditionVariable&) = delete;
+        ConditionVariable& operator=(const ConditionVariable&) = delete;
+        
+        void wait(Mutex& mutex);
+        template<typename Predict>
+        void wait(const Predict& predict, Mutex& mutex)
+        {
+            while(predict() == false)
+                m_condition.Wait(mutex);
+        }
+        
+        void notify_one();
+        void notify_all();
+        
+    private:
+        Condition m_condition;
+    };
+    
 }
